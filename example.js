e7f36d24b6597e83dca5c6e0430471b41883f365
Travis <travis.x.wong@gmail.com>
2019-02-26 17:55:26 +0800

    chapter 2

--- example.js
+++ example.js
@@ -6,9 +6,38 @@ var message = "The year is ";
 message += year;
 alert(message);
 */
+/*
 var a = 12, b = 20;
 if (a > b){
     alert("a > b");
 } else {
     alert("a < b");
 }
+*/
+/*
+var count = 1;
+
+while (count < 11) {
+    alert(count);
+    count++;
+}
+
+do {
+    alert(count);
+    count++;
+} while (count < 1);
+*/
+/*
+var beatles = Array("John", "Paul", "George", "Ringo");
+for (var count = 0; count < beatles.length; count++) {
+    alert(beatles[count]);
+}
+*/
+function square(num) {
+    var total = num * num;
+    return total;
+}
+var total = 50;
+var number = square(20);
+
+alert(total);
