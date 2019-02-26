2ddf1eb56851a4618d5033c59103d6402a413638
Travis <travis.x.wong@gmail.com>
2019-02-26 16:17:23 +0800

    compare

--- example.js
+++ example.js
@@ -1,6 +1,14 @@
 // var mood = "about 5'12\" tall";
 // alert(mood + ' ' + 20);
+/*
 var year = 2019;
 var message = "The year is ";
 message += year;
 alert(message);
+*/
+var a = 12, b = 20;
+if (a > b){
+    alert("a > b");
+} else {
+    alert("a < b");
+}
